2e5d118abec3f4c42b708c4517917979b1ceb4a3
Derrilong3 <ivan2003.185@gmail.com>
2022-09-17 15:56:55 +0300

    lab02

---
+++ lab02/Employee/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+
+            Employee employee1 = new Employee("Ivan", "Kryzhanivsky", Position.Developer, Experience.Junior);
+            Employee employee2 = new Employee("Pavel", "Morozov", Position.Manager);
+
+            Console.WriteLine(employee1.ToString());
+            Console.WriteLine(employee2.ToString());
+        }
+    }
+}
